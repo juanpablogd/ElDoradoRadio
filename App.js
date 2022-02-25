c65361b42ec0077688cbfb6596de633e44d8a843
Juan Pablo GD <juanpablogarzond@gmail.com>
2022-02-25 02:28:24 -0500

    Connect buttons - Icons

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -19,7 +20,12 @@ import {
   Animated,
   Easing,
   ImageBackground,
+  Linking,
+  Share,
 } from 'react-native';
+
+import { Icon } from 'react-native-elements';
+//LISTADO ICONOS https://oblador.github.io/react-native-vector-icons/
 
 import {
   Colors,
@@ -41,6 +47,7 @@ import TrackPlayer, {
 
 import imageBG from './img/FondoApp3-01.jpg';
 import imageRotate from './img/giratoria.png';
+import imgNotification from './img/PiezzaApp2-01.jpg';
 
 const App: () => Node = () => {
 
@@ -85,7 +92,7 @@ const App: () => Node = () => {
           url: 'http://51.81.49.98:8300/',
           title: 'El Dorado Radio',
           artist: 'Gobernación de Cundinamarca',
-          artwork: 'https://i.scdn.co/image/e5c7b168be89098eb686e02152aaee9d3a24e5b6',
+          artwork: imgNotification,
       });
 
       // Start playing it
@@ -94,6 +101,28 @@ const App: () => Node = () => {
       TrackPlayer.setVolume(0.4);
   };
   start();
+
+  const onShare = async () => {
+
+    let  urlShare = 'https://play.google.com/store/apps/details?id=com.skp.cundinamarca123&hl=es_CO&gl=US';
+    if (Platform.OS == 'ios') urlShare = 'https://www.apple.com/co/app-store/';
+    try {
+      const result = await Share.share({
+        message: urlShare,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -107,8 +136,40 @@ const App: () => Node = () => {
             transform: [{ rotate: RotateData }],
           }}
           source={imageRotate}
-        />
+      />
+
+      <Icon
+          reverse
+          name='facebook'
+          type='font-awesome'
+          color='#4267B2'
+          onPress={() => Linking.openURL('https://google.com')} />
+      <Icon
+          reverse
+          name='whatsapp'
+          type='font-awesome'
+          color='#25D366'
+          onPress={() => Linking.openURL('whatsapp://send?phone=+573003461962')} />
+      <Icon
+          reverse
+          name='phone'
+          type='font-awesome'
+          color='#FF5A5F'
+          onPress={() => Linking.openURL(`tel:3003461962`)} />
+      <Icon
+          reverse
+          name='share-alt'
+          type='font-awesome'
+          color='#0F9D58'
+          onPress={onShare} />
+      <Icon
+          reverse
+          name='web'
+          type='foundation'
+          onPress={() => Linking.openURL('http://www.eldoradoradio.co/inicio')} />
+
       </ImageBackground>
+
     </View>
   );
   
@@ -123,6 +184,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width: '125%',
     height:'103%'
+  },
+  icoFB:{
+    alignItems:'baseline'
   }
 });
 
